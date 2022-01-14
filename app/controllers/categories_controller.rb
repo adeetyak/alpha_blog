fa66316665adfebc470016486c1358593a71e63e
Aditya <mr.adityakaushal@gmail.com>
2022-01-14 15:41:32 +0530

    Categories added with CRUD

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [ :index, :show ]
   def show
-    end
+    @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 3)
+  end
   def create
     @category = Category.new(category_params)
     if @category.save
@@ -15,13 +18,32 @@ class CategoriesController < ApplicationController
 
   end
   def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
 
   end
-
+  def edit
+    @category = Category.find(params[:id])
+  end
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
   private
 
   def category_params
     params.require(:category).permit(:name)
+  end
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Onlu admins can perform that action"
+      redirect_to categories_path
+    end
+
   end
 
 end
